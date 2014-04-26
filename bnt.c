861e0f43ada6b785f578d98ab7cb687d62e8301b
nenad <neno.markus@gmail.com>
2014-04-26 10:28:46 +0200

    ...

--- bnt.c
+++ bnt.c
@@ -4,7 +4,7 @@
 
 #define _BINTEST_COORDINATES_NORMALIZATION_ 255
 
-int bintest(int32_t tcode, int threshold, int T[], int normalization, uint8_t pixels[], int nrows, int ncols, int ldim)
+int bintest(int32_t tcode, int threshold, int* T, int normalization, uint8_t* pixels, int nrows, int ncols, int ldim)
 {
 	int r1, c1, r2, c2;
 
@@ -14,11 +14,19 @@ int bintest(int32_t tcode, int threshold, int T[], int normalization, uint8_t pi
 	p = (int8_t*)&tcode;
 
 	//
+	/*
 	r1 = (T[0]*p[0] + T[1]*p[1] + T[2])/normalization;
 	c1 = (T[3]*p[0] + T[4]*p[1] + T[5])/normalization;
 
 	r2 = (T[0]*p[2] + T[1]*p[3] + T[2])/normalization;
 	c2 = (T[3]*p[2] + T[4]*p[3] + T[5])/normalization;
+	*/
+
+	r1 = (T[0]*p[0] + T[2])/_BINTEST_COORDINATES_NORMALIZATION_;
+	c1 = (T[4]*p[1] + T[5])/_BINTEST_COORDINATES_NORMALIZATION_;
+
+	r2 = (T[0]*p[2] + T[2])/_BINTEST_COORDINATES_NORMALIZATION_;
+	c2 = (T[4]*p[3] + T[5])/_BINTEST_COORDINATES_NORMALIZATION_;
 
 	//
 #ifndef USE_RGB
